9aaf50e255ba52192efda18a709040354c6a8164
rparks52 <rparks83097@gmail.com>
2023-07-26 09:26:42 -0400

    Added viewsets for booking and included user models.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
